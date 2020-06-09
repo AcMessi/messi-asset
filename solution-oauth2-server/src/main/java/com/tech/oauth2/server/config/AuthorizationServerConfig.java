ab0752532ec84480043752b7abaee2a7eccccb82
messi.chaoqun.wang <messi.chaoqun.wang@accenture.com>
2020-06-09 23:11:31 +0800

    udpate oauth2 server

--- solution-oauth2-server/src/main/java/com/tech/oauth2/server/config/AuthorizationServerConfig.java
+++ solution-oauth2-server/src/main/java/com/tech/oauth2/server/config/AuthorizationServerConfig.java
@@ -1,24 +1,11 @@
 package com.tech.oauth2.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-
-import javax.sql.DataSource;
 
 /**
  * @Description: 认证服务器配置类
@@ -32,35 +19,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    /**
-     * 刷新令牌
-     */
-    @Autowired
-    private UserDetailsService customUserDetailsService;
-
-    /**
-     * token管理方式，在TokenConfig类中已经添加到容器中了
-     */
-    @Autowired
-    private TokenStore tokenStore;
-
-    /**
-     * jwt转换器
-     */
-    @Autowired
-    private JwtAccessTokenConverter jwtAccessTokenConverter;
-
-    @Bean
-    public ClientDetailsService jdbcClientDetailsService() {
-        return new JdbcClientDetailsService(dataSource);
-    }
-
     /**
      * 配置被允许访问此认证服务器的客户端信息
      * 1.内存方式
@@ -71,49 +29,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        // jdbc管理客户端
-        clients.withClientDetails(jdbcClientDetailsService());
-    }
-
-    /**
-     * 授权码管理策略
-     *
-     * @return
-     */
-    @Bean
-    public AuthorizationCodeServices jdbcAuthorizationCodeServices() {
-        return new JdbcAuthorizationCodeServices(dataSource);
-    }
-
-    /**
-     * 关于认证服务器端点配置
-     *
-     * @param endpoints
-     * @throws Exception
-     */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        // password 要这个 AuthenticationManager 实例
-        endpoints.authenticationManager(authenticationManager);
-        // 刷新令牌时需要使用
-        endpoints.userDetailsService(customUserDetailsService);
-        // 令牌的管理方式
-        endpoints.tokenStore(tokenStore).accessTokenConverter(jwtAccessTokenConverter);
-        // 授权码管理策略 会产生的授权码放到 oauth_code 表中，如果这个授权码已经使用了，则对应这个表中的数据就会被删除
-        endpoints.authorizationCodeServices(jdbcAuthorizationCodeServices());
-    }
-
-    /**
-     * 令牌端点的安全配置
-     *
-     * @param security
-     * @throws Exception
-     */
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        // 认证后可访问 /oauth/token_key , 默认拒绝访问
-        security.tokenKeyAccess("permitAll()");
-        // 认证后可访问 /oauth/check_token , 默认拒绝访问
-        security.checkTokenAccess("isAuthenticated()");
+        // 基于内存模式管理客户端
+        clients.inMemory()
+                .withClient("test") // 客户端id
+                .secret(passwordEncoder.encode("test123")) // 加密，客户端密码
+                .authorizedGrantTypes("authorization_code", "password", "implicit", "client_credentials", "refresh_token")
+                .scopes("all") // 授权范围标识，哪部分资源可访问（all只是标识，不是说所有资源）
+                .autoApprove(false) // false 跳到一个授权页面手动点击授权，true不需要手动点授权，直接响应一个授权码
+                .redirectUris("http://locahost:9100")// 客户端回调地址
+                .accessTokenValiditySeconds(60 * 60 * 8) //访问令牌有效时长 默认为12小时
+                .refreshTokenValiditySeconds(60 * 60 * 24 * 60); // 刷新令牌有效时长,默认是30天
+//        http://localhost:8201/oauth/authorize?client_id=test&response_type=code
+//        http:localhost:8201/oauth/token
+//        Authorization: Basic client_id/client_secret
+//        code = KfeCcT & grant_type = authorization_code
     }
 }

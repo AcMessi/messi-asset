713756c7c38d6d7ff4d538ddc72c9d77bb55d56e
messi.chaoqun.wang <messi.chaoqun.wang@accenture.com>
2020-09-12 12:41:56 +0800

    update project structure

---
+++ solution-redis/src/main/java/com/tech/dao/UserMapper.java
@@ -0,0 +1,21 @@
+package com.tech.dao;
+
+import com.tech.model.User;
+import com.tech.model.UserKey;
+import com.tech.model.UserWithBLOBs;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(UserKey key);
+
+    int insert(UserWithBLOBs record);
+
+    int insertSelective(UserWithBLOBs record);
+
+    UserWithBLOBs selectByPrimaryKey(UserKey key);
+
+    int updateByPrimaryKeySelective(UserWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(UserWithBLOBs record);
+
+    int updateByPrimaryKey(User record);
+}
